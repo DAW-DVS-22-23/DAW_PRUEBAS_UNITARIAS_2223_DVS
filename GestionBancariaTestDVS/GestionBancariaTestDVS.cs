1174253d5a3827d8ee082b10625f574aff829f03
DVS-DAW-22-23 <valdesd1314@gmail.com>
2023-03-12 00:00:33 +0100

    Cambios hasta el punto 7 de la práctica.

---
+++ GestionBancariaTestDVS/GestionBancariaTestDVS.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GestionBancariaTestDVS
+{
+    [TestClass]
+    public class GestionBancariaTestDVS
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
