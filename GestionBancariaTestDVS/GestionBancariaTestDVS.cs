2ef012ff3fad54db64e48a3a93e44124c0b42e96
DVS-DAW-22-23 <valdesd1314@gmail.com>
2023-03-12 11:14:49 +0100

    Debemos dar algunos pasos atrás...

--- GestionBancariaTestDVS/GestionBancariaTestDVS.cs
+++ GestionBancariaTestDVS/GestionBancariaTestDVS.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using GestionBancariaAppNS;
+using System.Windows.Forms;
 
 namespace GestionBancariaTestDVS
 {
@@ -7,8 +9,22 @@ namespace GestionBancariaTestDVS
     public class GestionBancariaTestDVS
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidarReintegro()
         {
+            //Preparación de caso de prueba DVS 22/23
+            double saldoInicial = 1000;
+            double reintegro = 250;
+            double saldoEsperado = 750;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            //Método a probar DVS 22/23
+
+            miApp.RealizarReintegro(reintegro);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el reintegro, saldo incorrecto.");
         }
+
+
     }
 }
